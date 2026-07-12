2f7b7ed6215d6dc12079ce113496d3aeaf1b388f
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-12 19:44:28 +0300

    fix(accounts): fix RegisterForm clean() method

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,7 +15,7 @@ class RegisterForm(forms.ModelForm):
         fields = ['email', 'first_name', 'last_name', 'age', 'gender', 'profile_picture']
     
     def clean(self):
-        cleaned_data = super().clean
+        cleaned_data = super().clean()
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
         if (password and confirm_password) and (password != confirm_password):
