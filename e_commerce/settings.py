cd03246d401262e7ac7237b674c45217ca162291
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-12 17:46:54 +0300

    (chore): Adding static, media and tempalates dirs + static and media configs in settings!

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -114,8 +114,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+###################################### [Static Configuration] ######################################
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] # [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # BASE_DIR / 'staticfiles' # (Production)
 
-STATIC_URL = 'static/'
+###################################### [Media Configuration] ######################################
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # BASE_DIR / 'media' # (Production)
