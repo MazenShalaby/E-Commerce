c6d12857da97375e6036a4facadceed7730c19d3
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-12 19:13:43 +0300

    feat(accounts): add initial admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Account
+
 # Register your models here.
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'age', 'gender', 'is_active', 'date_joined', 'last_login']
+    search_fields = ['email', 'first_name', 'last_name']
+    list_filter = ['age', 'gender', 'is_active']
