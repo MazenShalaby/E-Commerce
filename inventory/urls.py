cce3ef1a2988d902d6787dd6ad866ddafe071673
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-13 18:00:32 +0300

    feat(inventory): add product search view, its route and rename its template

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import product_list, product_detail
+from .views import product_list, product_detail, product_search
 
 # create your urlpatterns here.
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('product-list/', product_list, name='product-list'),
     path('product-list/<slug:category_slug>/', product_list, name='categorized-product-list'),
     path('product-detail/<slug:product_slug>/', product_detail, name='product-detail'),
+    path('search/', product_search, name='product-search'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
 from .models import Category, Product
 
 # Create your views here.
@@ -25,3 +27,29 @@ def product_detail(request, product_slug):
     
     context = {'product': product}
     return render(request, 'inventory/product_detail.html', context)
+
+
+def product_search(request):
+    query = request.GET.get('query', '')
+    result = Product.objects.none()
+    result_count = 0
+    
+    if query:
+        search_vector = SearchVector('name', 'description', 'category__name')
+        search_query = SearchQuery(query)
+        search_rank = SearchRank(search_vector, search_query)
+        
+        result = Product.objects.annotate(
+            search=search_vector,
+            rank=search_rank
+        ).filter(
+            search=search_query,
+            quantity__gt=0
+        ).order_by('-rank')
+        result_count = result.count()
+    
+    context = {
+        'result': result,
+        'result_count': result_count,
+        }
+    return render(request, 'inventory/product_search.html', context)
