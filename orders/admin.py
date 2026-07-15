c8415653e96d6dbc307eac800df81577fa7e35c7
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-15 17:05:12 +0300

    feat(orders): Add initial models, migrations for accounts and register it in admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Order, OrderItem
+
 # Register your models here.
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderItemInline(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+    list_display = ['order_id', 'email', 'first_name', 'last_name', 'paid']
+    search_fields = ['order_id', 'email', 'first_name', 'last_name', 'city', 'address', 'postal_code']
+    list_filter = ['paid', 'created_at', 'updated_at']
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+import string, random
+from inventory.models import Product
 
 # Create your models here.
+
+
+def generate_order_id(length=16):
+    characters = string.ascii_letters + string.digits # str
+    random_characters = random.choices(characters, k=length) # list 
+    return "".join(random_characters) # str
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=16, unique=True, default=generate_order_id)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=30)
+    postal_code = models.PositiveIntegerField()
+    paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.order_id:
+            unique_order_id = generate_order_id()
+            while Order.objects.filter(order_id__iexact=unique_order_id).exists:
+                unique_order_id = generate_order_id()
+            self.order_id = unique_order_id
+        super().save(*args, **kwargs) # Call the real save() method
+    class Meta:
+        verbose_name = 'order'
+        verbose_name_plural = 'orders'
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['order_id']),
+            models.Index(fields=['paid']),
+            models.Index(fields=['-created_at']),
+        ]
+
+    def __str__(self):
+        return f"Order ID: {self.order_id}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.order_items.all())
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_items')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.id)
+    
+    def get_cost(self):
+        return self.price * self.quantity
