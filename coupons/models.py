b5a46c766cdd0a5931cd59662a70819dbb2f5884
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-18 17:29:21 +0300

    feat(coupons): add initial model and migration files!

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=10)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], help_text="Choose a number between 0 and 100")
+    active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.code # TODO
+
+    class Meta:
+        verbose_name = 'code'
+        verbose_name_plural = 'codes'
+        ordering = ['-created_at']
