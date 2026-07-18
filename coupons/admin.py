be3a87d0e0da558f7e52576a167ed5612a147830
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-18 17:33:03 +0300

    feat(coupons): add model admin representation!

--- coupons/admin.py
+++ coupons/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Coupon
+
 # Register your models here.
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active', 'created_at', 'updated_at']
+    search_fields = ['code', 'discount']
+    list_filter = ['valid_from', 'valid_to', 'discount', 'active', 'created_at', 'updated_at']
