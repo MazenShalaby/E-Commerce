30389958d0b9af9e57bf9da3a8d4a701212cd6a7
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-25 17:55:41 +0300

    refactor(coupons): refactor Coupon model

--- coupons/models.py
+++ coupons/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Coupon(models.Model):
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=10, unique=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)], help_text="Choose a number between 0 and 100")
