38d0be649c89ae36ee2aa53685146af45dafa030
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-25 17:28:35 +0300

    feat(cart): add initial cart models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+
+from inventory.models import Product
+
 
 # Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="cart"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        related_name="items",
+        on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
