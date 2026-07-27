d6d9eca474cb93c9afaadd2e0eecb67ac8731cb7
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-27 15:34:40 +0300

    feat(cart): add str dunder method for Cart model + admin representaion

--- cart/models.py
+++ cart/models.py
@@ -20,6 +20,10 @@ class Cart(models.Model):
         on_delete=models.SET_NULL,
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.first_name + self.user.last_name}'s Cart"
+    
 class CartItem(models.Model):
     cart = models.ForeignKey(
         Cart,
