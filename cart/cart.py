f53cf319114e00fad435543a12f57df51068da67
Mazen Shalaby <waleedmazen85@gmail.com>
2026-07-27 14:37:57 +0300

    refactor(cart): refactor Cart add method!

--- cart/cart.py
+++ cart/cart.py
@@ -25,17 +25,34 @@ class Cart:
         self.session.modified = True
 
     def add(self, product, quantity=1, override_quantity=False):
-        product_id = str(product.id)
-        item = self.cart.setdefault(
-            product_id, {"quantity": 0, "price": str(product.price)}
-        )
 
-        if override_quantity:
-            item["quantity"] = quantity
-        else:
-            item["quantity"] += quantity
+            if self.request.user.is_authenticated:
+                item, created = CartItem.objects.get_or_create(
+                    cart=self.db_cart,
+                    product=product,
+                    defaults={"quantity": 0},
+                )
 
-        self.save()
+                if override_quantity:
+                    item.quantity = quantity
+                else:
+                    item.quantity += quantity
+
+                item.save()
+
+            else:
+                product_id = str(product.id)
+                item = self.cart.setdefault(
+                    product_id,
+                    {"quantity": 0, "price": str(product.price)},
+                )
+
+                if override_quantity:
+                    item["quantity"] = quantity
+                else:
+                    item["quantity"] += quantity
+
+                self.save()
 
     def _clear_coupon_if_cart_empty(self):
         if not self.cart:
