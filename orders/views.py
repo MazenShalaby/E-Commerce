cbbaf76bc55ab19f16cd617923424b37987cbb61
Mazen Shalaby <waleedmazen85@gmail.com>
2026-08-03 16:37:11 +0300

    style(orders): remove unnecessary imports

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.mail import send_mail
 from django.conf import settings
 from django.http import HttpResponse
-import json
 
 from .forms import OrderCreateForm
 from .models import Order, OrderItem
