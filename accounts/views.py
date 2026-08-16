b06d4225e08072fad0c20932237c71bf24d6fcac
Mazen Shalaby <waleedmazen85@gmail.com>
2026-08-16 22:27:40 +0300

    style: remove unused imports

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.core.mail import EmailMessage
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode, urlencode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
